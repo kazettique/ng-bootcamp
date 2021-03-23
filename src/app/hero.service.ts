474f15780212a0dd966dd10d3c7391a558e1ee00
Woody <woody.chang@cloud-interactive.com>
2021-03-23 16:55:45 +0800

    Finish: 4. Add Services

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+// Mock data
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
+// Interface
+import { Hero } from './hero';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
 }
