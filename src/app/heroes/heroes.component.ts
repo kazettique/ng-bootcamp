72021827bcf36014d3bc82baf995ee3b1c429897
Woody.Chang <kazettique@gmail.com>
2021-03-24 18:43:54 +0800

    Add dynamic route with angular router

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,11 @@
+// Angular
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'
+
+// Interface
+import { Hero } from '../hero';
+
+// Service
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,17 +15,12 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
 
-  heroes: Hero[] = []
+  heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
