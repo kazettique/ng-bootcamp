27f55697a07738e5dd2f2993d2c0710d4311266c
Woody.Chang <kazettique@gmail.com>
2021-03-25 15:04:12 +0800

    Finish: 6. Get Data from a Server

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -23,9 +23,26 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
+  // get heroes list
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
+  }
+
+  // add new hero
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  // delete hero
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
   }
 
 }
